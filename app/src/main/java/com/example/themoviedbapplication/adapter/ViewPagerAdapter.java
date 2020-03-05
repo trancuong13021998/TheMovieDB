f6737a606722e396b3ffa438adc684678f17b5be
trancuong13021998 <trancuong13021998@users.noreply.github.com>
2020-03-05 23:57:29 +0700

    commit 04

---
+++ app/src/main/java/com/example/themoviedbapplication/adapter/ViewPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.example.themoviedbapplication.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private final ArrayList<Fragment> fragmentArrayList = new ArrayList<>();
+    private final ArrayList<String> fragmentTile = new ArrayList<>();
+    public ViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentArrayList.get(position);
+    }
+    @Override
+    public int getCount() {
+        return fragmentArrayList.size();
+    }
+    public void addFragment(Fragment fragment,String title){
+        fragmentArrayList.add(fragment);
+        fragmentTile.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTile.get(position);
+    }
+}
--- app/src/main/java/com/example/themoviedbapplication/view/MainActivity.java
+++ app/src/main/java/com/example/themoviedbapplication/view/MainActivity.java
@@ -1,16 +1,33 @@
 package com.example.themoviedbapplication.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.example.themoviedbapplication.R;
+import com.example.themoviedbapplication.adapter.ViewPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = (TabLayout)findViewById(R.id.tab_layout_main);
+        viewPager = (ViewPager)findViewById(R.id.view_page_main);
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    public void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new MovieFragment(),"Movie");
+        viewPagerAdapter.addFragment(new TVShowsFragment(),"TV Shows");
+        this.viewPager.setAdapter(viewPagerAdapter);
     }
 }
